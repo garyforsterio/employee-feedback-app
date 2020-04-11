f765e24e075853701ec5f170a62f953849e5794f
Gary Forster <gary@garyforster.io>
2020-04-11 19:47:39 +0900

    feat: move docker compose file to root and add dummy data

--- packages/client/src/constants.ts
+++ packages/client/src/constants.ts
@@ -2,7 +2,7 @@
 export const GRAVATAR_URL = 'https://www.gravatar.com/avatar/';
 
 // Server API BASE
-export const API_BASE = 'http://localhost:3000';
+export const API_BASE = 'http://localhost/api';
 
 // Used to store token in local storage
 export const LOCAL_STORAGE_TOKEN_KEY = 'token';
